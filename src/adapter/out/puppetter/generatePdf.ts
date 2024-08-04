49a1fe2f9c07af5bb9916120b33ce08be5e12c08
ss49919201 <sakashin10291029@gmail.com>
2024-08-04 13:43:33 +0900

    ローカルファイルをPDF化

--- src/adapter/out/puppetter/generatePdf.ts
+++ src/adapter/out/puppetter/generatePdf.ts
@@ -8,24 +8,49 @@ import {
 const generatePdf = async (
   input: GeneratePdfInput
 ): Promise<GeneratePdfOutput> => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://news.ycombinator.com", {
-    waitUntil: "networkidle2",
+  const content = readContent();
+
+  await writePdf({
+    content,
+    outputPath: input.name,
   });
 
-  await page.pdf({
-    path: input.name,
-  });
-
-  await browser.close();
-
-  const buffer = readFileSync(input.name);
-  unlinkSync(input.name);
+  const buffer = pdfToBuffer(input.name);
+  removePdf(input.name);
 
   return {
     buffer,
   };
+};
+
+const readContent = (): string => {
+  const contentPath = "./src/adapter/out/puppetter/asset/index.html";
+  const content = readFileSync(contentPath, "utf-8");
+  return content;
+};
+
+const writePdf = async ({
+  content,
+  outputPath,
+}: {
+  content: string;
+  outputPath: string;
+}): Promise<void> => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(content);
+  await page.pdf({
+    path: outputPath,
+  });
+  await browser.close();
+};
+
+const pdfToBuffer = (pdfPath: string): Buffer => {
+  return readFileSync(pdfPath);
+};
+
+const removePdf = (pdfPath: string): void => {
+  unlinkSync(pdfPath);
 };
 
 export default generatePdf;
